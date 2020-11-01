f7984d51b313a819d8b9b80c28bffdf101a5fe5d
Shashank Verma <shashankverma1816@gmail.com>
2020-11-01 19:05:14 +0530

    Facially it's recognising-videocapture added

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -36,5 +36,21 @@ while True:
     success,img=cap.read()
     imgS=cv2.resize(img,(0,0),None,0.25,0.25)#We want to reduce the size of the image to speed up the process the scale is 0.25 i.e one-fourth of the original size
     imgS=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)# We'll convert the image to rgb
-    #Now we can find multiple images in our 
 
+    #Now we can find multiple images in our webcam so for that we need to find the locations of our images and then we'll send in the locations of our images to the encoding function
+    #to find the location
+    facesCurFrame= face_recognition.face_locations(imgS)
+    encodesCurFrame=face_recognition.face_encodings(imgS,facesCurFrame)
+    #we'll iterate through all the faces in our current frame
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame): #one by one it'll grab one face location and it's encoding
+        #then we'll perform the matching
+        matches=face_recognition.compare_faces(encodeListKnown,encodeFace)
+        #then we'll find the distance
+        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)#The lowest distance will be our best match
+        print(faceDis)
+        matchIndex=np.argmin(faceDis) # to get the image with the lowest distance
+
+
+    #We can create a bounding box around the face and then we'll print the name
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
